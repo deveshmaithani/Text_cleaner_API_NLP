455414cb29a400cdc897f49044a1b9475ce82173
deveshmaithani <deveshmaithani4545@gmail.com>
2025-07-20 22:49:41 +0530

    FastAPI implemented

--- app.py
+++ app.py
@@ -1,16 +1,32 @@
-from flask import Flask, request, jsonify
+# from flask import Flask, request, jsonify
+# from text_cleaner import TextCleaner
+
+# app = Flask(__name__)
+# cleaner = TextCleaner()
+
+# @app.route("/clean", methods=["POST"])
+# def clean_text():
+#     data = request.get_json()
+#     paragraph = data.get("text", "")
+
+#     cleaned = cleaner.clean(paragraph)
+#     return jsonify({"cleaned_sentences": cleaned})
+
+# if __name__ == "__main__":
+#     app.run(debug=True)
+
+
+from fastapi import FastAPI
+from pydantic import BaseModel
 from text_cleaner import TextCleaner
 
-app = Flask(__name__)
+app = FastAPI()
 cleaner = TextCleaner()
 
-@app.route("/clean", methods=["POST"])
-def clean_text():
-    data = request.get_json()
-    paragraph = data.get("text", "")
+class TextIn(BaseModel):
+    text: str
 
-    cleaned = cleaner.clean(paragraph)
-    return jsonify({"cleaned_sentences": cleaned})
-
-if __name__ == "__main__":
-    app.run(debug=True)
+@app.post("/clean")
+async def clean_text(data: TextIn):
+    cleaned = cleaner.clean(data.text)
+    return {"cleaned_text": cleaned}
